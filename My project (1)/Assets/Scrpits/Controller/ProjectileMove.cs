f6ba4f4a9746714757457a8b9361bd4a3c71257f
mker23 <kmk32989@gmail.com>
2023-05-08 12:49:58 +0900

    0508_적 생성

--- My project (1)/Assets/Scrpits/Controller/ProjectileMove.cs
+++ My project (1)/Assets/Scrpits/Controller/ProjectileMove.cs
@@ -16,11 +16,26 @@ public class ProjectileMove : MonoBehaviour
         if (collision.gameObject.name == "Monster")
         {
             collision.gameObject.GetComponent<MonsterController>().Damaged(1);
+            Destroy(this.gameObject);
         }
     }
     private void FixedUpdate()
     {
         float moveAmount = 3 * Time.fixedDeltaTime;
         transform.Translate(launchDirection * moveAmount);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Wall")
+        {
+            Destroy(this.gameObject);
+        }
+
+        if (other.gameObject.tag == "Monster")
+        {
+            other.gameObject.GetComponent<MonsterController>().Damaged(1);
+            Destroy(this.gameObject);
+        }
     }
 }
